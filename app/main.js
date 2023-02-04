b6dfb4112d1814e4a94283c8c1f3652e62ed2102
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-02-04 11:33:57 -0300

    map method

--- app/main.js
+++ app/main.js
@@ -3,28 +3,10 @@ let books = []
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 
 getSearchAPIBooks()
-const insertBooksElement = document.getElementById('livros')
 
 async function getSearchAPIBooks() {
   const aws = await fetch(endpointAPI)
   books = await aws.json()
-  console.table(books)
-  showBooksOnScreen(books)
-}
-
-function showBooksOnScreen(booksList) {
-  booksList.forEach(book => {
-    insertBooksElement.innerHTML += `
-    <div class="livro">
-      <img class="livro__imagens" src="${book.imagem}" ${book.alt}/>
-      <h2 class="livro__titulo">
-        ${book.titulo}
-      </h2>
-      <p class="livro__descricao">${book.autor}</p>
-      <p class="livro__preco" id="preco">R$${book.preco}</p>
-      <div class="tags">
-        <span class="tag">${book.categoria}</span>
-      </div>
-    </div>`
-  });
+  let discountBooks = applyDiscount(books)
+  showBooksOnScreen(discountBooks)
 }
